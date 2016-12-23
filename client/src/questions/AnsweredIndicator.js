0f1ab5e9d2bdb7a3884731bc9d888e9ac77dccc1
Fredrik Teschke <f@ftes.de>
2016-12-23 01:09:30 +0100

    add tooltip for answered indicator

--- client/src/questions/AnsweredIndicator.js
+++ client/src/questions/AnsweredIndicator.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 import FA from 'react-fontawesome'
 
 const AskQuestion = ({ answered }) => (
-  answered ? <FA name='thumbs-o-up'/> : null
+  answered ? <FA name='thumbs-o-up' title='Knows the answer.'/> : null
 )
 
 
