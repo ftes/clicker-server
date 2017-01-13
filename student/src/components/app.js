158ecea30ac20996e2f64563e1396f2bd6b1c43a
Fredrik Teschke <f@ftes.de>
2017-01-13 12:26:08 +0100

    move five-click into own component

--- student/src/components/app.js
+++ student/src/components/app.js
@@ -1,5 +1,4 @@
 import React, { PropTypes } from 'react'
-import { Subject } from 'rxjs'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
@@ -9,55 +8,31 @@ import BatteryLevel from '../battery-level/battery-level'
 import LocalStorage from '../save/local-storage'
 import WebSocket from '../websocket/websocket'
 import LoadSettings from '../settings/load-settings'
+import FiveClick from '../settings/five-click'
 
-export class App extends React.Component {
-  /**
-   * On 5-click show settings.
-   */
-  componentDidMount() {
-    const clickStream = new Subject()
-
-    clickStream
-      .bufferWhen(() => clickStream.debounceTime(200)) // 200ms
-      .map(list => list.length)
-      .filter(length => length === 5) // 5 clicks
-      .subscribe(() => this.props.toggleShowSettings())
-
-    this.clickStream = clickStream
-  }
-
-  onClick() {
-    this.clickStream.next()
-  }
-
-  render() {
-    const { showSettings } = this.props
-    return (
-      <div onClick={() => this.onClick()}>
-        <div className='content' style={{
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-        }}>
-          {showSettings &&
-            <Settings/>
-          }
-          {!showSettings &&
-            <Buttons/>
-          }
-        </div>
-        <LoadSettings/>
-        <BatteryLevel/>
-        <LocalStorage/>
-        <WebSocket/>
-      </div>
-    )
-  }
-}
+export const App = ({ showSettings }) => (
+  <FiveClick>
+    <div className='content' style={{
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+    }}>
+      {showSettings &&
+        <Settings/>
+      }
+      {!showSettings &&
+        <Buttons/>
+      }
+    </div>
+    <LoadSettings/>
+    <BatteryLevel/>
+    <LocalStorage/>
+    <WebSocket/>
+  </FiveClick>
+)
 
 App.propTypes = {
   showSettings: PropTypes.bool.isRequired,
-  toggleShowSettings: PropTypes.func.isRequired,
 }
 
 export default App
--- student/src/components/appC.js
+++ student/src/components/appC.js
@@ -1,15 +1,12 @@
 import { connect } from 'react-redux'
 import App from './app'
 import { getState as settings } from '../settings'
-import { toggleShow as toggleShowSettings } from '../settings'
 
 
 const mapStateToProps = (state) => ({
   showSettings: settings(state).show,
 })
 
-const mapDispatchToProps = (dispatch) => ({
-  toggleShowSettings: () => dispatch(toggleShowSettings()),
-})
+const mapDispatchToProps = () => ({})
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
