ba5958e6d8af77fcb163f83103551f3beb5f6f0a
Fredrik Teschke <f@ftes.de>
2017-01-14 02:44:12 +0100

    remove obsolete scripts

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-(cd teacher; npm run build)
-rm -rf server/public/teacher
-cp -r teacher/build server/public/teacher
--- install.sh
+++
@@ -1,6 +0,0 @@
-apt update
-apt install git nodejs
-git clone https://github.com/ftes/clicker-server.git
-cd clicker-server/server
-npm install
-nodejs index.js --web=4001
