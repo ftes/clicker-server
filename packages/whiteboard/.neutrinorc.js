89daa3c5c49cfed183fa6f06290b1c4378b813ac
Fredrik Teschke <fteschke@wescale.com>
2018-03-01 20:39:05 +0100

    [fix] unit tests (add enyme adapter and configure jest)

--- packages/whiteboard/.neutrinorc.js
+++ packages/whiteboard/.neutrinorc.js
@@ -1,6 +1,12 @@
 module.exports = {
   use: [
     '@neutrinojs/react',
+    [
     '@neutrinojs/jest',
+      {
+        testRegex: `\\.test\\.js$`,
+        setupTestFrameworkScriptFile: './test-setup.js',
+      },
+    ],
   ]
 };
