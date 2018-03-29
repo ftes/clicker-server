3017287ea9319b6172503737231df27293936655
Fredrik Teschke <f@ftes.de>
2018-03-29 23:07:13 +0200

    [fix] display of id mappings modal

--- packages/common/src/id-mappings/id-mappings-modal.jsx
+++ packages/common/src/id-mappings/id-mappings-modal.jsx
@@ -20,19 +20,26 @@ class IdMappingsModal extends React.Component {
     const { show } = this.state;
 
     return show && ReactDOM.createPortal(
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">ID Mappings</h5>
-            <button type="button" className="close">
-              <span>&times;</span>
-            </button>
-          </div>
-          <div className="modal-body">
-            <IdMappings {...this.props} />
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" onClick={this.close}>Close</button>
+      <div
+        className="modal"
+        style={{
+          display: show ? 'block' : undefined,
+        }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">ID Mappings</h5>
+              <button type="button" className="close" onClick={this.close}>
+                <span>&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <IdMappings {...this.props} />
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" onClick={this.close}>Close</button>
+            </div>
           </div>
         </div>
       </div>,
